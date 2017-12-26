e53ecd852cbb37b4878c71ac390f2946754ade81
Keuni <32663961+Keuny@users.noreply.github.com>
2017-12-27 00:13:04 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ jQuery(document).ready(function($){
             $('body').toggleClass('mobile-menu-active');
             $('#mobile-menu-button i').toggleClass('icon-cancel icon-menu-2');
             $('#mobile-body-overlay').toggle();
-            $(this).toggle(function(){
+            $('body').toggle(function(){
                 $('body').on('scroll touchmove mousewheel', function (e) {
                     e.preventDefault();
                     e.stopPropagation();
