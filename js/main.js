589485b2437a1ab84f6ee8708b73c7585cb14bab
Keuni <32663961+Keuny@users.noreply.github.com>
2017-12-26 22:30:55 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -29,18 +29,23 @@ jQuery(document).ready(function($){
             $('body').toggleClass('mobile-menu-active');
             $('#mobile-menu-button i').toggleClass('icon-cancel icon-menu-2');
             $('#mobile-body-overlay').toggle()
-        })
-        
-        if ($('body').hasClass('mobile-menu-active')) {
             $('body').on('scroll touchmove mousewheel', function (e) {
                 e.preventDefault();
                 e.stopPropagation();
                 return false;
-            });
-        }
-        else {
-            $('body').off('scroll touchmove mousewheel');
-        }   
+            })
+        })
+        
+        // if ($('body').hasClass('mobile-menu-active')) {
+        //     $('body').on('scroll touchmove mousewheel', function (e) {
+        //         e.preventDefault();
+        //         e.stopPropagation();
+        //         return false;
+        //     });
+        // }
+        // else {
+        //     $('body').off('scroll touchmove mousewheel');
+        // }   
       
         $(document).on('click', function(e){
             var container = $("#mobile-menu, #mobile-menu-button");
