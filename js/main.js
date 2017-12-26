5ef3992bbcb71c4b6a10f30be54ac6634b727ae0
Keuni <32663961+Keuny@users.noreply.github.com>
2017-12-26 22:04:35 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ jQuery(document).ready(function($){
                 }); 
             } 
             else {
-                $('body').off('scroll touchmove mousewheel');
+                $('body').unbind('scroll touchmove mousewheel');
             }
         })   
       
