66d4a561bef8c521abf1ba8cf72ecf680e21a994
Keuni <32663961+Keuny@users.noreply.github.com>
2017-12-27 14:32:20 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -30,10 +30,11 @@ jQuery(document).ready(function($){
             $('body').toggleClass('mobile-menu-active');
             $('#mobile-menu-button i').toggleClass('icon-cancel icon-menu-2');
             $('#mobile-body-overlay').toggle();
-            if (('body').hasClass('mobile-menu-active')){
-                $('body').on('scroll touchmove mousewheel', function (e) {
-                    e.preventDefault();
-                    e.stopPropagation();
+            if ((document).hasClass('mobile-menu-active')){
+                $('body').on('scroll touchmove mousewheel', function (event) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    return false;
                 })
             }
             else{
